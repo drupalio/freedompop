a3973e3a263e074c0e4ed9427ca45d411ecd2b12
Ricardo Morales Paredes <ricardo.morales.paredes.contractor@bbva.com>
2019-03-07 16:55:47 -0600

    add routes

---
+++ src/main/java/com/freedompop/facade/impl/FreedomPopFacade.java
@@ -0,0 +1,10 @@
+package com.freedompop.facade.impl;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.freedompop.facade.IFredomPopFacade;
+
+@RestController("/")
+public class FreedomPopFacade  implements IFredomPopFacade{
+
+}
