61582f938818828a464707fd71bc36ec3b9e64b1
Ricardo Morales Paredes <ricardo.morales.paredes.contractor@bbva.com>
2019-03-08 19:58:34 -0600

    crud

--- src/main/java/com/freedompop/persons/dao/TokenRepository.java
+++ src/main/java/com/freedompop/persons/dao/TokenRepository.java
@@ -2,7 +2,7 @@ package com.freedompop.persons.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.freedompop.login.entities.TokenEntity;
+import com.freedompop.persons.entities.TokenEntity;
 
 public interface TokenRepository extends CrudRepository<TokenEntity,Integer> {
 
